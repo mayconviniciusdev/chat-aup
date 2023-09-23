4cfffb97584d48d0f921b889b51a723c27a8410a
Maycon Vinicius <contato@mayconviniciusdev.com>
2023-09-22 22:33:33 -0300

    Using contexts and reducer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
 
+import { ChatProvider } from "@/contexts/chatContext";
+import { UserProvider } from "@/contexts/userContext";
+
 const Page = () => {
   return (
     <div>
+      <UserProvider>
+        <ChatProvider>
+          <h1>chatAUP</h1>
+        </ChatProvider>
+      </UserProvider>
     </div>
   )
 }
